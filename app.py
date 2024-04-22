d2d004585cf3a46307e4ac92d5570972bc260ec0
Bibek Rai <bibekrai655@gmail.com>
2024-04-22 12:38:07 +0545

    typo fixed

--- app.py
+++ app.py
@@ -38,21 +38,22 @@ def recommend_similar_artists(artist_name, df, top_n=3):
     if len(matching_artists) == 0:
         print("No artist found with a similar name.")
         return
+    
     input_artist_info = matching_artists.iloc[0]
+
+    input_genre = input_artist_info['Genres']
     
-    input_genre = input_artist_info['genres_type']
+    similar_genre_artists = df[df['Genres'] == input_genre]
+
+    similar_artists = similar_genre_artists[similar_genre_artists['Artist Name'].str.lower() != artist_name]
     
-    similar_genre_artists = df[df['genres_type'] == input_genre]
-    
-    similar_genre_artists = similar_genre_artists[similar_genre_artists['Artist Name'].str.lower() != artist_name]
-    
-    similar_artists = similar_genre_artists.head(top_n)
+    similar_artists = similar_artists.head(top_n)
     
     return similar_artists
 
 @app.route('/suggest', methods=['POST'])
 def suggest():
-    artist_name = request.json['artistName']
+    artist_name = request.json['artistname']
     similar_artists = recommend_similar_artists(artist_name, df)
     suggested_artist_names = similar_artists['Artist Name'].tolist()
     return jsonify({'artistNames': suggested_artist_names})
