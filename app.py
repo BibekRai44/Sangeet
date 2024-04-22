644f78bee32cbec6e09a184445affbf5ef577ba7
Bibek Rai <bibekrai655@gmail.com>
2024-04-22 09:11:12 +0545

    directly defining function instead of using pickle

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify,url_for
 import pickle
+import pandas as pd
 app = Flask(__name__)
 
 model=pickle.load(open('predict_artist.pkl','rb'))
@@ -12,6 +13,10 @@ def index():
 def artist_name_predictor():
     return render_template('artist_name_predictor.html')
 
+@app.route('/suggest_artist')
+def suggest_artist():
+    return render_template('artist_suggestor.html')
+
 @app.route('/predict', methods=['POST'])
 def predict():
     
@@ -22,6 +27,35 @@ def predict():
     predicted_artist_name = prediction[0]
 
     return jsonify({'artistName': predicted_artist_name})
+
+df = pd.read_csv("new_data.csv")
+
+def recommend_similar_artists(artist_name, df, top_n=3):
+    artist_name = artist_name.lower()
+    
+    matching_artists = df[df['Artist Name'].str.lower().str.contains(artist_name)]
+    
+    if len(matching_artists) == 0:
+        print("No artist found with a similar name.")
+        return
+    input_artist_info = matching_artists.iloc[0]
+    
+    input_genre = input_artist_info['genres_type']
+    
+    similar_genre_artists = df[df['genres_type'] == input_genre]
+    
+    similar_genre_artists = similar_genre_artists[similar_genre_artists['Artist Name'].str.lower() != artist_name]
+    
+    similar_artists = similar_genre_artists.head(top_n)
+    
+    return similar_artists
+
+@app.route('/suggest', methods=['POST'])
+def suggest():
+    artist_name = request.json['artistName']
+    similar_artists = recommend_similar_artists(artist_name, df)
+    suggested_artist_names = similar_artists['Artist Name'].tolist()
+    return jsonify({'artistNames': suggested_artist_names})
 
 if __name__ == '__main__':
     app.run(debug=True)
